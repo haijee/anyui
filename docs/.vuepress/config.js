98dfc036d139ff1ef85283fab4932932dc439387
haijie.guo <haijie.guo@net263.com>
2022-05-20 19:26:23 +0800

    feat:theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,19 @@
+
+const { defaultTheme } = require('vuepress')
+
+
 module.exports = {
     lang: 'zh-CN',
     title: 'anyui', 
     description: 'anyui的博客',
     base:'/anyui/', //配合项目部署路径
+    theme: defaultTheme({
+        // 默认主题配置
+        navbar: [
+          {
+            text: '首页',
+            link: '/',
+          },
+        ],
+      }),
 }
