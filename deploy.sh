963dad851d99b9b6ca08b972196db0e9e0dec6be
haijie.guo <haijie.guo@net263.com>
2022-05-20 19:00:35 +0800

    config

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 ​
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:haijee/anyui.git master:gh-pages
+git push -f git@github.com:haijee/anyui.git master
 
 cd -
