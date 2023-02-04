d5f62965b4ab9362ca298a37772a252291c143fe
郭海杰 <haijee@qq.com>
2023-02-05 00:42:08 +0800

    feat

---
+++ 柯里化.js
@@ -0,0 +1,23 @@
+const add = function (x, y, z) {
+  return x + y + z;
+};
+
+const curry = function (fn) {
+  return function curried(...args) {
+    console.log(fn.length, args.length);
+    //这里不能用箭头函数
+    if (fn.length === args.length) {
+      return fn.apply(this, args);
+    } else {
+      return function (...args1) {
+        return curried.apply(this, args.concat(args1));
+      };
+    }
+  };
+};
+
+const curry_add = curry(add);
+const next = curry_add(111);
+const next1 = next(6);
+const next2 = next1(10);
+console.log(next2);
