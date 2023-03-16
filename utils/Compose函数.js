3a7c9936dc008162900681a98b6fc7fe5f46d582
郭海杰 <haijee@qq.com>
2023-03-17 00:18:38 +0800

    feat

--- utils/Compose函数.js
+++ utils/Compose函数.js
@@ -1,10 +1,15 @@
-// https://www.cnblogs.com/qianxiaox/p/13679185.html
 
+
+// 参考文档：
+// https://www.cnblogs.com/qianxiaox/p/13679185.html
+// https://blog.51cto.com/u_15283585/2959721
+
+// 组合函数 一般是从右向左的顺序
 
 const add = x => x + 10;
 const multiply = x => x * 10;
 
-const compose = function(){
+const compose1 = function(){
   // 将接收的参数存到一个数组， args == [multiply, add]
   const args = [].slice.apply(arguments);
   return function(x) {
@@ -12,8 +17,21 @@ const compose = function(){
   }
 }
 
+// 异步
+const compose2 = function (...args) {
+  let init = args.pop();
+  return function (...arg) {
+    return args.reverse().reduce(function (sequence, func) {
+      return sequence.then(function (result) {
+        return func.call(null, result);
+      });
+    }, Promise.resolve(init.apply(null, arg)));
+  };
+};
+
+
 // 简介版
-function compose1(...fns) {
+function compose3(...fns) {
   return fns.reduce((f, g) => (...args) => {
     return f(g(...args));
   });
