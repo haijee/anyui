c2118940204abedb8ac6790e16f50ae8a6469b88
郭海杰 <32607669+haijee@users.noreply.github.com>
2023-03-16 16:37:10 +0800

    Update 实现promise函数.js

--- 实现promise函数.js
+++ 实现promise函数.js
@@ -178,3 +178,76 @@ new MyPromise(resolve => {
   })
   .then(res2 => console.log(res2));
 
+
+
+
+
+//Promise/A+规范的三种状态
+const PENDING = 'pending';
+const FULFILLED = 'fulfilled';
+const REJECTED = 'rejected';
+
+class MyPromise {
+  // 构造方法接收一个回调
+  constructor(executor) {
+    this._status = PENDING; // Promise状态
+    this._resolveQueue = []; // 成功队列, resolve时触发
+    this._rejectQueue = []; // 失败队列, reject时触发
+
+    // 由于resolve/reject是在executor内部被调用, 因此需要使用箭头函数固定this指向, 否则找不到this._resolveQueue
+    let _resolve = val => {
+      if (this._status !== PENDING) return; // 对应规范中的"状态只能由pending到fulfilled或rejected"
+      this._status = FULFILLED; // 变更状态
+
+      // 这里之所以使用一个队列来储存回调,是为了实现规范要求的 "then 方法可以被同一个 promise 调用多次"
+      // 如果使用一个变量而非队列来储存回调,那么即使多次p1.then()也只会执行一次回调
+      while (this._resolveQueue.length) {
+        const callback = this._resolveQueue.shift();
+        callback(val);
+      }
+    };
+    // 实现同resolve
+    let _reject = val => {
+      if (this._status !== PENDING) return; // 对应规范中的"状态只能由pending到fulfilled或rejected"
+      this._status = REJECTED; // 变更状态
+      while (this._rejectQueue.length) {
+        const callback = this._rejectQueue.shift();
+        callback(val);
+      }
+    };
+    // new Promise()时立即执行executor,并传入resolve和reject
+    executor(_resolve, _reject);
+  }
+
+  // then方法,接收一个成功的回调和一个失败的回调
+  then(resolveFn, rejectFn) {
+    switch (this._status) {
+      case PENDING:
+        // ‘PENDING’状态下向回调函数集合添加callback
+        this._resolveQueue.push(resolveFn);
+        this._rejectQueue.push(rejectFn);
+        break;
+      case RESOLVED:
+        // 将回调函数的返回值赋值给 实例的 value，满足链式调用then方法时传递value
+        this.value = f(this.value);
+        break;
+      case REJECTED:
+        // 同上
+        this.value = r(this.value);
+        break;
+      default:
+        break;
+    }
+    // 满足链式调用then，返回MyPromise实例对象
+    return this;
+  }
+}
+
+const p1 = new MyPromise((resolve, reject) => {
+  setTimeout(() => {
+    resolve('result');
+  }, 1000);
+});
+p1.then(res => console.log(res)).then(res => console.log(res));
+
+
